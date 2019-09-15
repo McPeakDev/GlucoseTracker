9009084456b1db2ea190847df7c0ffc2db440760
Zachery Johnson <johnsonzd@etsu.edu>
2019-09-15 15:24:14 -0400

    New App Created, New Login Page with entry options for Username and password, new buttons for login and create account. No Functionality yet just a shell

---
+++ GlucoseTrackerApp/GlucoseTrackerApp/GlucoseTrackerApp/ObjectClasses/Patient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlucoseTrackerApp.ObjectClasses
+{
+    class Patient : User
+    {
+        public override int UserID { get; set; }
+        public override string UserName { get; set; }
+        public override string FirstName { get; set; }
+        public override string LastName { get; set; }
+        public override string Email { get; set; }
+        public override string ContactNumber { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Doctor"/> class.
+        /// </summary>
+        /// <param name="ID">The ID used in the DataBase for this user.</param>
+        /// <param name="UName">User Name.</param>
+        /// <param name="FName">First Name.</param>
+        /// <param name="LName">Last Name.</param>
+        /// <param name="EMail">Email.</param>
+        /// <param name="CNumb">Contact Number.</param>
+        public Patient(int ID, string UName, string FName, string LName, string EMail, string CNumb)
+        {
+            this.UserID = ID;
+            this.UserName = UName;
+            this.FirstName = FName;
+            this.LastName = LName;
+            this.Email = EMail;
+            this.ContactNumber = CNumb;
+        }
+    }
+}
---
+++ GlucoseTrackerApp/GlucoseTrackerApp/GlucoseTrackerApp/Pages/AccountCreation.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GlucoseTrackerApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AccountCreation : ContentPage
+	{
+		public AccountCreation ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
