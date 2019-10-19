9c7a57e1bbf0ea25e66a21905deed736d8913f07
ZacheryJohnson <johnsonzd@etsu.edu>
2019-10-19 12:25:29 -0400

    commit early so matt can merge

--- GlucoseTrackerApp(New)/GlucoseTrackerApp/Resources/Resource.designer.cs
+++ GlucoseTrackerApp(New)/GlucoseTrackerApp/Resources/Resource.designer.cs
@@ -2366,14 +2366,14 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080033
 			public const int CTRL = 2131230771;
 			
-			// aapt resource value: 0x7f080092
-			public const int ExerciseDashboard = 2131230866;
+			// aapt resource value: 0x7f080093
+			public const int ExerciseDashboard = 2131230867;
 			
 			// aapt resource value: 0x7f080034
 			public const int FUNCTION = 2131230772;
 			
-			// aapt resource value: 0x7f080093
-			public const int FakeGraphImage = 2131230867;
+			// aapt resource value: 0x7f080094
+			public const int FakeGraphImage = 2131230868;
 			
 			// aapt resource value: 0x7f080035
 			public const int META = 2131230773;
@@ -2384,8 +2384,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f0800b6
-			public const int action0 = 2131230902;
+			// aapt resource value: 0x7f0800b7
+			public const int action0 = 2131230903;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2408,17 +2408,17 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int action_container = 2131230899;
+			// aapt resource value: 0x7f0800b4
+			public const int action_container = 2131230900;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800ba
-			public const int action_divider = 2131230906;
+			// aapt resource value: 0x7f0800bb
+			public const int action_divider = 2131230907;
 			
-			// aapt resource value: 0x7f0800b4
-			public const int action_image = 2131230900;
+			// aapt resource value: 0x7f0800b5
+			public const int action_image = 2131230901;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2435,14 +2435,14 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800cc
-			public const int action_settings = 2131230924;
+			// aapt resource value: 0x7f0800cd
+			public const int action_settings = 2131230925;
 			
-			// aapt resource value: 0x7f0800b5
-			public const int action_text = 2131230901;
+			// aapt resource value: 0x7f0800b6
+			public const int action_text = 2131230902;
 			
-			// aapt resource value: 0x7f0800c3
-			public const int actions = 2131230915;
+			// aapt resource value: 0x7f0800c4
+			public const int actions = 2131230916;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2450,8 +2450,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080027
 			public const int add = 2131230759;
 			
-			// aapt resource value: 0x7f080098
-			public const int add_exercise_button = 2131230872;
+			// aapt resource value: 0x7f080099
+			public const int add_exercise_button = 2131230873;
 			
 			// aapt resource value: 0x7f080070
 			public const int alertTitle = 2131230832;
@@ -2468,8 +2468,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080044
 			public const int auto = 2131230788;
 			
-			// aapt resource value: 0x7f08009b
-			public const int auto_login = 2131230875;
+			// aapt resource value: 0x7f08009c
+			public const int auto_login = 2131230876;
 			
 			// aapt resource value: 0x7f08002f
 			public const int beginning = 2131230767;
@@ -2483,8 +2483,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800b7
-			public const int cancel_action = 2131230903;
+			// aapt resource value: 0x7f0800b8
+			public const int cancel_action = 2131230904;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2498,8 +2498,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800bf
-			public const int chronometer = 2131230911;
+			// aapt resource value: 0x7f0800c0
+			public const int chronometer = 2131230912;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2510,14 +2510,14 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080039
 			public const int collapseActionView = 2131230777;
 			
-			// aapt resource value: 0x7f0800a5
-			public const int container = 2131230885;
+			// aapt resource value: 0x7f0800a6
+			public const int container = 2131230886;
 			
 			// aapt resource value: 0x7f080066
 			public const int contentPanel = 2131230822;
 			
-			// aapt resource value: 0x7f0800a6
-			public const int coordinator = 2131230886;
+			// aapt resource value: 0x7f0800a7
+			public const int coordinator = 2131230887;
 			
 			// aapt resource value: 0x7f08006d
 			public const int custom = 2131230829;
@@ -2531,44 +2531,44 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080060
 			public const int default_activity_button = 2131230816;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int design_bottom_sheet = 2131230888;
+			// aapt resource value: 0x7f0800a9
+			public const int design_bottom_sheet = 2131230889;
+			
+			// aapt resource value: 0x7f0800b0
+			public const int design_menu_item_action_area = 2131230896;
 			
 			// aapt resource value: 0x7f0800af
-			public const int design_menu_item_action_area = 2131230895;
+			public const int design_menu_item_action_area_stub = 2131230895;
 			
 			// aapt resource value: 0x7f0800ae
-			public const int design_menu_item_action_area_stub = 2131230894;
+			public const int design_menu_item_text = 2131230894;
 			
 			// aapt resource value: 0x7f0800ad
-			public const int design_menu_item_text = 2131230893;
-			
-			// aapt resource value: 0x7f0800ac
-			public const int design_navigation_view = 2131230892;
+			public const int design_navigation_view = 2131230893;
 			
 			// aapt resource value: 0x7f080020
 			public const int disableHome = 2131230752;
 			
-			// aapt resource value: 0x7f0800a2
-			public const int doctor_id = 2131230882;
+			// aapt resource value: 0x7f0800a3
+			public const int doctor_id = 2131230883;
 			
 			// aapt resource value: 0x7f08008b
 			public const int drawer_layout = 2131230859;
 			
-			// aapt resource value: 0x7f08008d
-			public const int drawer_layout_register = 2131230861;
+			// aapt resource value: 0x7f08008e
+			public const int drawer_layout_register = 2131230862;
 			
 			// aapt resource value: 0x7f08007e
 			public const int edit_query = 2131230846;
 			
-			// aapt resource value: 0x7f080099
-			public const int email = 2131230873;
+			// aapt resource value: 0x7f08009a
+			public const int email = 2131230874;
 			
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800c5
-			public const int end_padder = 2131230917;
+			// aapt resource value: 0x7f0800c6
+			public const int end_padder = 2131230918;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2576,11 +2576,11 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080040
 			public const int enterAlwaysCollapsed = 2131230784;
 			
-			// aapt resource value: 0x7f080094
-			public const int exercise_add_button = 2131230868;
+			// aapt resource value: 0x7f080095
+			public const int exercise_add_button = 2131230869;
 			
-			// aapt resource value: 0x7f080097
-			public const int exercise_hours = 2131230871;
+			// aapt resource value: 0x7f080098
+			public const int exercise_hours = 2131230872;
 			
 			// aapt resource value: 0x7f080041
 			public const int exitUntilCollapsed = 2131230785;
@@ -2600,8 +2600,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080048
 			public const int fill_vertical = 2131230792;
 			
-			// aapt resource value: 0x7f08009e
-			public const int first_name = 2131230878;
+			// aapt resource value: 0x7f08009f
+			public const int first_name = 2131230879;
 			
 			// aapt resource value: 0x7f080054
 			public const int @fixed = 2131230804;
@@ -2618,14 +2618,14 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080021
 			public const int homeAsUp = 2131230753;
 			
-			// aapt resource value: 0x7f080096
-			public const int hours_prompt = 2131230870;
+			// aapt resource value: 0x7f080097
+			public const int hours_prompt = 2131230871;
 			
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800c4
-			public const int icon_group = 2131230916;
+			// aapt resource value: 0x7f0800c5
+			public const int icon_group = 2131230917;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2633,11 +2633,11 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int imageView = 2131230897;
+			// aapt resource value: 0x7f0800b2
+			public const int imageView = 2131230898;
 			
-			// aapt resource value: 0x7f0800c0
-			public const int info = 2131230912;
+			// aapt resource value: 0x7f0800c1
+			public const int info = 2131230913;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2645,11 +2645,11 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
 			
-			// aapt resource value: 0x7f0800a4
-			public const int largeLabel = 2131230884;
+			// aapt resource value: 0x7f0800a5
+			public const int largeLabel = 2131230885;
 			
-			// aapt resource value: 0x7f0800a0
-			public const int last_name = 2131230880;
+			// aapt resource value: 0x7f0800a1
+			public const int last_name = 2131230881;
 			
 			// aapt resource value: 0x7f080049
 			public const int left = 2131230793;
@@ -2666,26 +2666,26 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f08009c
-			public const int login_button = 2131230876;
+			// aapt resource value: 0x7f08009d
+			public const int login_button = 2131230877;
 			
-			// aapt resource value: 0x7f080095
-			public const int logout_button = 2131230869;
+			// aapt resource value: 0x7f080096
+			public const int logout_button = 2131230870;
 			
-			// aapt resource value: 0x7f0800c8
-			public const int masked = 2131230920;
+			// aapt resource value: 0x7f0800c9
+			public const int masked = 2131230921;
 			
-			// aapt resource value: 0x7f0800b9
-			public const int media_actions = 2131230905;
+			// aapt resource value: 0x7f0800ba
+			public const int media_actions = 2131230906;
 			
-			// aapt resource value: 0x7f0800c6
-			public const int message = 2131230918;
+			// aapt resource value: 0x7f0800c7
+			public const int message = 2131230919;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
 			
-			// aapt resource value: 0x7f08009f
-			public const int middle_name = 2131230879;
+			// aapt resource value: 0x7f0800a0
+			public const int middle_name = 2131230880;
 			
 			// aapt resource value: 0x7f080053
 			public const int mini = 2131230803;
@@ -2693,20 +2693,23 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080028
 			public const int multiply = 2131230760;
 			
-			// aapt resource value: 0x7f0800cb
-			public const int nav_bloodsugar = 2131230923;
-			
-			// aapt resource value: 0x7f0800c9
-			public const int nav_exercise = 2131230921;
+			// aapt resource value: 0x7f0800cc
+			public const int nav_bloodsugar = 2131230924;
 			
 			// aapt resource value: 0x7f0800ca
-			public const int nav_food = 2131230922;
+			public const int nav_exercise = 2131230922;
+			
+			// aapt resource value: 0x7f0800cb
+			public const int nav_food = 2131230923;
 			
 			// aapt resource value: 0x7f08008c
 			public const int nav_view_dashboard = 2131230860;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int navigation_header_container = 2131230891;
+			// aapt resource value: 0x7f08008d
+			public const int nav_view_exercise_add = 2131230861;
+			
+			// aapt resource value: 0x7f0800ac
+			public const int navigation_header_container = 2131230892;
 			
 			// aapt resource value: 0x7f08003b
 			public const int never = 2131230779;
@@ -2717,14 +2720,14 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800c2
-			public const int notification_background = 2131230914;
+			// aapt resource value: 0x7f0800c3
+			public const int notification_background = 2131230915;
+			
+			// aapt resource value: 0x7f0800bd
+			public const int notification_main_column = 2131230909;
 			
 			// aapt resource value: 0x7f0800bc
-			public const int notification_main_column = 2131230908;
-			
-			// aapt resource value: 0x7f0800bb
-			public const int notification_main_column_container = 2131230907;
+			public const int notification_main_column_container = 2131230908;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2735,11 +2738,11 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08000b
 			public const int parent_matrix = 2131230731;
 			
-			// aapt resource value: 0x7f08009a
-			public const int password = 2131230874;
+			// aapt resource value: 0x7f08009b
+			public const int password = 2131230875;
 			
-			// aapt resource value: 0x7f0800a1
-			public const int phone_number = 2131230881;
+			// aapt resource value: 0x7f0800a2
+			public const int phone_number = 2131230882;
 			
 			// aapt resource value: 0x7f08004d
 			public const int pin = 2131230797;
@@ -2753,17 +2756,17 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080075
 			public const int radio = 2131230837;
 			
-			// aapt resource value: 0x7f08009d
-			public const int register_button = 2131230877;
+			// aapt resource value: 0x7f08009e
+			public const int register_button = 2131230878;
 			
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800c1
-			public const int right_icon = 2131230913;
+			// aapt resource value: 0x7f0800c2
+			public const int right_icon = 2131230914;
 			
-			// aapt resource value: 0x7f0800bd
-			public const int right_side = 2131230909;
+			// aapt resource value: 0x7f0800be
+			public const int right_side = 2131230910;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2837,14 +2840,14 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080025
 			public const int showTitle = 2131230757;
 			
-			// aapt resource value: 0x7f0800a3
-			public const int smallLabel = 2131230883;
+			// aapt resource value: 0x7f0800a4
+			public const int smallLabel = 2131230884;
+			
+			// aapt resource value: 0x7f0800ab
+			public const int snackbar_action = 2131230891;
 			
 			// aapt resource value: 0x7f0800aa
-			public const int snackbar_action = 2131230890;
-			
-			// aapt resource value: 0x7f0800a9
-			public const int snackbar_text = 2131230889;
+			public const int snackbar_text = 2131230890;
 			
 			// aapt resource value: 0x7f080043
 			public const int snap = 2131230787;
@@ -2867,8 +2870,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800b8
-			public const int status_bar_latest_event_content = 2131230904;
+			// aapt resource value: 0x7f0800b9
+			public const int status_bar_latest_event_content = 2131230905;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2894,11 +2897,11 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080069
 			public const int textSpacerNoTitle = 2131230825;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int textView = 2131230898;
+			// aapt resource value: 0x7f0800b3
+			public const int textView = 2131230899;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int text_input_password_toggle = 2131230896;
+			// aapt resource value: 0x7f0800b1
+			public const int text_input_password_toggle = 2131230897;
 			
 			// aapt resource value: 0x7f080014
 			public const int textinput_counter = 2131230740;
@@ -2906,8 +2909,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800be
-			public const int time = 2131230910;
+			// aapt resource value: 0x7f0800bf
+			public const int time = 2131230911;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2918,17 +2921,17 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08006f
 			public const int title_template = 2131230831;
 			
-			// aapt resource value: 0x7f080090
-			public const int toolbar = 2131230864;
-			
-			// aapt resource value: 0x7f08008e
-			public const int toolbar_dashboard = 2131230862;
+			// aapt resource value: 0x7f080091
+			public const int toolbar = 2131230865;
 			
 			// aapt resource value: 0x7f08008f
-			public const int toolbar_exercise_add = 2131230863;
+			public const int toolbar_dashboard = 2131230863;
 			
-			// aapt resource value: 0x7f080091
-			public const int toolbar_register = 2131230865;
+			// aapt resource value: 0x7f080090
+			public const int toolbar_exercise_add = 2131230864;
+			
+			// aapt resource value: 0x7f080092
+			public const int toolbar_register = 2131230866;
 			
 			// aapt resource value: 0x7f08003e
 			public const int top = 2131230782;
@@ -2936,8 +2939,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f08006e
 			public const int topPanel = 2131230830;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int touch_outside = 2131230887;
+			// aapt resource value: 0x7f0800a8
+			public const int touch_outside = 2131230888;
 			
 			// aapt resource value: 0x7f08000f
 			public const int transition_current_scene = 2131230735;
@@ -2966,8 +2969,8 @@ namespace GlucoseTrackerApp
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800c7
-			public const int visible = 2131230919;
+			// aapt resource value: 0x7f0800c8
+			public const int visible = 2131230920;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
